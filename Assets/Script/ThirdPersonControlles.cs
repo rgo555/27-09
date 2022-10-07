bb520680a27f6a3563b4e352b17ae44990e83913
rgo555 <95858479+rgo555@users.noreply.github.com>
2022-10-07 10:34:25 +0200

    Personaje_Correr_3D

--- Assets/Script/ThirdPersonControlles.cs
+++ Assets/Script/ThirdPersonControlles.cs
@@ -7,6 +7,7 @@ public class ThirdPersonControlles : MonoBehaviour
     private CharacterController controller; 
     private Vector3 playerVelocity;
     public Transform cam;
+    public Transform LookAtTransform;
 
 
     public Transform groundSensor;
@@ -16,21 +17,22 @@ public class ThirdPersonControlles : MonoBehaviour
     public float speed = 5; 
     public float jumpForce = 20;
     public float jumpHeight =1;
-    private float gravity = -12f;
+    private float gravity = -12f;  // -9.81f
     public bool isGrounded; 
-// -9.81f
 
+    public Cinemachine.AxisState xAxis;
+    public Cinemachine.AxisState yAxis;
+    
     private float turnSmoothVelocity; 
     public float turnSmoothTime = 0.1f; 
-    void Awake()
 
+    void Awake()
     {
         controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-     void Movement()
-
+    void Movement()
     {
          Vector3 move = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
@@ -44,9 +46,9 @@ public class ThirdPersonControlles : MonoBehaviour
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
     }
+
     //moviento tps con freelock camera
     void MovementTPS()
-
     {
          Vector3 move = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
@@ -60,24 +62,30 @@ public class ThirdPersonControlles : MonoBehaviour
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
     }
+
     //Moviento con camra fija
     void MovementTPS2()
+    {
+        Vector3 move = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
-     {
-         Vector3 move = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        xAxis.Update(Time.deltaTime);
+        yAxis.Update(Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(0, xAxis.Value, 0);
+        LookAtTransform.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, LookAtTransform.eulerAngles.z);
 
         if(move != Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0, angle, 0);
+            
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
     }
-    void Update()
 
+    void Update()
     {
         //Movement();
         //MovementTPS();
@@ -98,5 +106,5 @@ public class ThirdPersonControlles : MonoBehaviour
         }
         playerVelocity.y += gravity * Time.deltaTime;
         controller.Move(playerVelocity* Time.deltaTime);
-        }
+    }
 }
