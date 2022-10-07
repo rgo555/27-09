b2d74dd3084e334d11ca6e425152a3814e223bc2
rgo555 <95858479+rgo555@users.noreply.github.com>
2022-10-07 08:51:03 +0200

    Caminar_Personjae_3D

--- Assets/Script/ThirdPersonControlles.cs
+++ Assets/Script/ThirdPersonControlles.cs
@@ -6,6 +6,7 @@ public class ThirdPersonControlles : MonoBehaviour
 {
     private CharacterController controller; 
     private Vector3 playerVelocity;
+    public Transform cam;
 
 
     public Transform groundSensor;
@@ -18,25 +19,69 @@ public class ThirdPersonControlles : MonoBehaviour
     private float gravity = -12f;
     public bool isGrounded; 
 // -9.81f
+
+    private float turnSmoothVelocity; 
+    public float turnSmoothTime = 0.1f; 
     void Awake()
+
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+     void Movement()
+
     {
-        Vector3 move = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+         Vector3 move = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
         if(move != Vector3.zero)
         {
-            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
 
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
-
-            controller.Move(move * speed * Time.deltaTime);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+    }
+    //moviento tps con freelock camera
+    void MovementTPS()
 
+    {
+         Vector3 move = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
+        if(move != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+    //Moviento con camra fija
+    void MovementTPS2()
+
+     {
+         Vector3 move = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        if(move != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, cam.eulerAngles.y, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+    void Update()
+
+    {
+        //Movement();
+        //MovementTPS();
+        MovementTPS2();
        // isGrounded = controller.isGrounded;
         isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
         if(isGrounded && playerVelocity.y < 0)
